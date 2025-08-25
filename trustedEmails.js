22331ab807b2deb62d70aacc0dcc31bf9fa73605
Sanchez-hub <aleksrahimov2000@gmail.com>
2025-08-25 17:34:50 +0300

    trusted emails with regex

---
+++ trustedEmails.js
@@ -0,0 +1,18 @@
+var arr = [
+  { userName:"Test", lastName:"Test", email:"test.test@gmail.com" },
+  { userName:"Dmitro", lastName:"Porohov", email:"dmitro.porohov@yahoo.com" },
+  { userName:"Andrii", lastName:"", email:"andrii@mail.ru" }
+];
+
+var result = [];
+
+var re = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@(gmail\.com|yahoo\.com)$/;
+
+for (var i = 0; i < arr.length; i++) {
+  var email = arr[i].email;
+  if (re.test(email)) {
+    result.push(email);
+  }
+}
+
+console.log(result);
